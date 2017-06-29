cb47804fb66f1727e76f0382922307051f7cae7c
zhoudunyang <zhoudunyang@meituan.com>
2017-06-29 21:32:03 +0800

    新增code
    新增code

--- src/main/java/com/dylan326/justcode/C4LIS.java
+++ src/main/java/com/dylan326/justcode/C4LIS.java
@@ -2,6 +2,7 @@ package com.dylan326.justcode;
 
 /**
  * Created by dylan on 2017/6/13.
+ * 最长递增子串个数
  */
 public class C4LIS {
     public static int LIS(int[] a) {
@@ -26,7 +27,51 @@ public class C4LIS {
         return tmp;
     }
 
+    /**
+     * 倒序输出所有结果
+     *
+     * @param a
+     * @return
+     */
+    public static int[][] LISArray(int[] a) {
+        int[] maxLength = new int[a.length];
+        int[][] result = new int[a.length][a.length];
+
+        for (int i = 0; i < a.length; i++) {
+            maxLength[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (a[j] < a[i] && maxLength[i] <= maxLength[j] + 1) {
+                    maxLength[i] = maxLength[j] + 1;
+                }
+            }
+        }
+        int tmp = 0;
+        for (int i = 0; i < maxLength.length; i++) {
+            if (maxLength[i] > tmp) {
+                tmp = maxLength[i];
+            }
+        }
+
+        for (int j = maxLength.length - 1; j >= 0; j--) { // 找到结果，开始递减的位置
+            int flag = tmp;
+            if (maxLength[j] != tmp) { //第一个最长位置才是后续代码的起点
+                continue;
+            }
+            for (int i = j; i >= 0; i--) {
+                if (maxLength[i] == flag) {
+                    System.out.print(a[i]);
+                    flag--;
+                }
+            }
+            System.out.println("");
+        }
+
+
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println(LIS(new int[]{1,3,2,5,7,4,10}));
+        LIS(new int[]{1, 7, 8, 3, 4});
+        LISArray(new int[]{1, 7, 8, 3, 4});
     }
 }
