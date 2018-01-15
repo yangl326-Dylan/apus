713385649a2e176f5cb16df8585d3b319a34df40
zhoudunyang <zhoudunyang@meituan.com>
2018-01-15 16:16:24 +0800

    添加A11 test

--- src/main/java/com/dylan326/apus/A11MostContainer.java
+++ src/main/java/com/dylan326/apus/A11MostContainer.java
@@ -3,7 +3,6 @@ package com.dylan326.apus;
 /**
  * Created by dylan on 2017/9/21.
  * 两个边的构成一个矩形，为初始面积， 比他大的只可能是短边被替换，因为delta X变小，如果出现面积更大，只可能是比短边更长的边
- *
  */
 public class A11MostContainer {
     public int maxArea(int[] height) {
@@ -25,6 +24,7 @@ public class A11MostContainer {
 
 
     public static void main(String[] args) {
-
+        A11MostContainer a11MostContainer = new A11MostContainer();
+        System.out.println(a11MostContainer.maxArea(new int[]{1, 6, 3, 4}));
     }
 }
