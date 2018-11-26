3df514f113886f6327daa6f434b5e6a64aa9eceb
zhoudunyang <zhoudunyang@meituan.com>
2018-11-26 14:45:53 +0800

    调整NextPermutation

--- src/main/java/com/dylan326/apus/A31NextPermutation.java
+++ src/main/java/com/dylan326/apus/A31NextPermutation.java
@@ -8,52 +8,57 @@ import java.util.Arrays;
  * 1,4,3,2 -> 2,1,3,4
  * 1,3,4,2 -> 1,4,2,3
  */
-    public class A31NextPermutation {
+public class A31NextPermutation {
 
-    public static int[] next(int[] num) {
+    public static int[] next(int[] nums) {
         int tmp1 = -1;
         int index1 = 0;
-        for (int i = num.length - 1; i > 0; i--) {//记录从右边开始查找，找到的第一个左邻位小的数
-            if (num[i - 1] < num[i]) {
-                tmp1 = num[i - 1];
+        for (int i = nums.length - 1; i > 0; i--) {//记录从右边开始查找，找到的第一个左邻位小的数
+            if (nums[i - 1] < nums[i]) {
+                tmp1 = nums[i - 1];
                 index1 = i - 1;
                 break;
             }
         }
         if (tmp1 == -1) { // 找不到则为字典序的最大值， 下一个按照题要求为回到最小值
 //            return num;
-
-            return Arrays;
+            int m = 0, n = nums.length - 1;
+            while (m < n) {
+                int tmp = nums[m];
+                nums[m] = nums[n];
+                nums[n] = tmp;
+            }
+            return nums;
         }
         int delta = Integer.MAX_VALUE;
         int index2 = 0;
-        for (int i = index1 + 1; i < num.length; i++) {//找到比tmp1大的最小数
-            if (num[i] >= tmp1 && num[i] - tmp1 < delta) {
-                delta = num[i] - tmp1;
+        for (int i = index1 + 1; i < nums.length; i++) {//找到比tmp1大的最小数
+            if (nums[i] >= tmp1 && nums[i] - tmp1 < delta) {
+                delta = nums[i] - tmp1;
                 index2 = i;
 
             }
         }
         //swap
-        num[index1] = num[index2];
-        num[index2] = tmp1;
+        nums[index1] = nums[index2];
+        nums[index2] = tmp1;
 
         //sort index1+1~ num.lengh
-        for (int i = index1+1; i <num.length ; i++) {// 对index1后面的数据按小到大排序
-            for (int j = i+1; j <num.length; j++) {
-                if(num[i]>num[j]){
-                    int tmp = num[i];
-                    num[i] = num[j];
-                    num[j] = tmp;
+        for (int i = index1 + 1; i < nums.length; i++) {// 对index1后面的数据按小到大排序
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = tmp;
                 }
             }
         }
-        return num;
+        return nums;
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(next(new int[]{4,3,2,1})));
-        System.out.println(Arrays.toString(next(new int[]{1,3,2,4})));
+        System.out.println(Arrays.toString(next(new int[]{4, 3, 2, 1})));
+        System.out.println(Arrays.toString(next(new int[]{1, 3, 2, 4})));
     }
 
 }
