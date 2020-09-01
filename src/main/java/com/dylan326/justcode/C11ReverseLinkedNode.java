92de248dc1f7e44076edab0332b6680bfef74efd
zhoudunyang <zhoudunyang@meituan.com>
2020-09-01 10:57:19 +0800

    添加C11

---
+++ src/main/java/com/dylan326/justcode/C11ReverseLinkedNode.java
@@ -0,0 +1,26 @@
+package com.dylan326.justcode;
+
+import com.dylan326.apus.A19RemoveNthNodeEnd.ListNode;
+
+/**
+ * Created by dylan on 2019/11/11.
+ * 链表反转
+ * Given 1->2->3->4->5->6, you should return the list as 6->5->4->3->2->1.
+ */
+public class C11ReverseLinkedNode {
+
+    public ListNode reverseLinkNode(ListNode sourceNode) {
+        return  null;
+    }
+
+    public static void main(String[] args) {
+        switch (1) {
+            case 1:
+                System.out.println(1111);
+            default:
+                System.out.println(222);
+        }
+
+
+    }
+}
