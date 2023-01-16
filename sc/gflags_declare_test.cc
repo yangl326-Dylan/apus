b5c8e9049025472e35e244fe6519dc384b382ed2
zhoudunyang <zhoudunyang@meituan.com>
2023-01-16 11:39:41 +0800

    gfalgs 工具库代码

---
+++ sc/gflags_declare_test.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <gflags/gflags.h>
+
+//declare
+static bool ValidatePort(const char* flagname, int32_t value);
+void print_message();
+
+// define flags
+DEFINE_string(msg, "default", "The message to print");
+DEFINE_int32(port, 80, "need a port, between 0 and 255");
+DEFINE_bool(l, false, "open l args, default false");
+DEFINE_bool(n, true, "open n args, default true");
+
+DEFINE_validator(port, &ValidatePort);// inject validator
+
+/**
+ * gflags 工具库学习
+ * main test. usage doc: https://gflags.github.io/gflags/
+*/
+int main(int argc, char **argv)
+{
+  // GFLAGS_NAMESPACE::SetUsageMessage("Test compilation and use of gflags_declare.h");
+  GFLAGS_NAMESPACE::ParseCommandLineFlags(&argc, &argv, true);
+  print_message();
+  return 0;
+}
+
+/**
+ *  validate flag
+*/
+bool ValidatePort(const char* flagname, int32_t value) {
+   if (value >= 0 && value < 256)   // value is ok
+     return true;
+   printf("Invalid value for --%s:%d, port should be between 0 and 255\n", flagname, (int)value);
+   return false;
+}
+
+/**
+ * print flags
+*/
+void print_message()
+{
+  std::cout << "message:"<< FLAGS_msg << std::endl;
+  std::cout << "port:" << FLAGS_port << std::endl;
+  std::cout << "l:" << FLAGS_l << std::endl;
+  std::cout << "n:" << FLAGS_n << std::endl;
+}
+
---
+++ sc/tutorial.cc
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        return 1;
+    }
+
+    // convert input to double
+    const double inputValue = atof(argv[1]);
+
+    // calculate square root
+    const double outputValue = sqrt(inputValue);
+    std::cout << "The square root of " << inputValue
+              << " is " << outputValue
+              << std::endl;
+    return 0;
+}
