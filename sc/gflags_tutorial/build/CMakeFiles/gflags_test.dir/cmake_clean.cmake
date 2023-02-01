9553a04f20725b4026ee1ad9935ab6b0db9a4eff
zhoudunyang <zhoudunyang@meituan.com>
2023-02-01 11:30:40 +0800

    google gflags 工具库代码学习

---
+++ sc/gflags_tutorial/build/CMakeFiles/gflags_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gflags_test.dir/gflags_declare_test.cc.o"
+  "gflags_test.pdb"
+  "gflags_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gflags_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
