74fad58d33ca5249c02aebbe6190402eee13b178
zhoudunyang <zhoudunyang@meituan.com>
2023-07-19 14:37:19 +0800

    test PB  thrift

---
+++ src/main/java/com/dylan326/tmp/TestPB.java
@@ -0,0 +1,7 @@
+package com.dylan326.tmp;
+
+/**
+ * Created by dylan on 2023/5/31.
+ */
+public class TestPB {
+}
---
+++ src/main/java/com/dylan326/tmp/testProto.thrift
@@ -0,0 +1,16 @@
+namespace java com.dylan326.tmp
+
+typedef i64 long
+typedef i32 int
+typedef i16 short
+
+struct Data{
+    1:long uid;
+    2:short appId;
+    3:bool useAccess
+}
+
+struct DataNew{
+    1:long uid;
+    2:short appId;
+}
